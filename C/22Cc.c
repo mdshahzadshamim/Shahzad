16e68b841cb9959d7002ee2d7aab19e8ae2e78a0
Shahzad <shahzad.shamim@hotmail.com>
2024-01-12 12:45:54 +0530

    Completed sorting part of question: 22Cc

--- C/22Cc.c
+++ C/22Cc.c
@@ -11,6 +11,8 @@ typedef struct datemonthyear
     unsigned int year: 12;
 }dmy;
 
+int compare(const void *pa, const void *pb);
+
 int main()
 {
     srand(time(NULL));
@@ -22,8 +24,17 @@ int main()
         date[i].year = 2000 + rand() % 24;
     }
 
+    qsort(date, 10, sizeof(dmy), compare);
+
     printf("\nDay\tMonth\tYear\n\n");
     for(int i = 0; i < COUNT; i++)
     printf("%u\t%u\t%d\n", date[i].day, date[i].month, date[i].year);
     return 0;
+}
+
+int compare(const void *pa, const void *pb)
+{
+    const dmy *a = pa;
+    const dmy *b = pb;
+    return a->year - b->year;
 }
